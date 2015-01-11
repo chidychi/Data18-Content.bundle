737ba63ae120dfffd9e3cd283b9c64763035fc1e
chidychi <chidychi@hotmail.com>
2015-01-11 16:30:58 -0500

    Update __init__.py
    Small fix that will allow certain /dev actresses to appear in searches.

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -4,7 +4,7 @@ import random
 
 # this code was borrowed from the Excalibur Films Agent. April 9 2013
 # URLS
-VERSION_NO = '1.2014.07.13.1'
+VERSION_NO = '1.2015.01.11.1'
 EXC_BASEURL = 'http://www.data18.com/'
 EXC_SEARCH_MOVIES = EXC_BASEURL + 'search/?k=%s&t=0'
 EXC_MOVIE_INFO = EXC_BASEURL + 'content/%s'
@@ -41,7 +41,12 @@ def search_na(results, media_title, year, lang):
   xp = "//select//Option[text()[contains(translate(., '%s', '%s'), '%s')]]//@value" % (na_url_site.upper(), na_url_site.lower(), na_url_site.lower())
   Log('xPath: ' + xp)
   try:
-    searchURL = search_results.xpath(xp)[0]
+      try:
+      searchURL = search_results.xpath(xp)[0]
+      except:
+      xp = "//select//Option[text()[contains(translate(., '%s', '%s'), '%s')]]//@value" % (na_url_site.upper(), na_url_site.lower(), na_url_site.lower())
+      Log('xPath: ' + xp)
+      searchURL = search_results.xpath(xp)[0]
   except:
     search_results = HTML.ElementFromURL(searchURL + '/sites/')
     xp = "//a[text()[contains(translate(., '%s', '%s'), '%s')]]//@href" % (na_url_site.upper(), na_url_site.lower(), na_url_site.lower())
